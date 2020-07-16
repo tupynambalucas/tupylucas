38568ef4d94a2351fd578ce01fdd364441fada76
tupynambalucas <53620332+tupynambalucas@users.noreply.github.com>
2020-07-16 02:00:46 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var rellax = new Rellax('.container-texto');
+var rellax = new Rellax('.i-1');
+var rellax = new Rellax('.i-2');
+var rellax = new Rellax('.i-3');
+var rellax = new Rellax('.i-4');
+var rellax = new Rellax('.i-5');
+var rellax = new Rellax('.i-6');
+
